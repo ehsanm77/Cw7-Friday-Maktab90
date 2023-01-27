ed6816daaef80c0326ebeca296cbb00e17b56186
ehsanm <ehsanmohamadi780@gmail.com>
2023-01-27 11:20:47 +0330

    Answer Q2

--- script.js
+++ script.js
@@ -57,5 +57,58 @@ let people = [
 
 // console.log(people);
 
+// Q2-Js
+// let char = "hello i'm a student and search my problems in google";
+// let result = '';
 
+// <with for of>
+
+// for (let str of char) {
+// switch (str) {
+//   case 'a':
+//     result += '@';
+//     break;
+//   case 'e':
+//     result += '3';
+//     break;
+//   case 'g':
+//     result += '&';
+//     break;
+//   case 'i':
+//     result += '!';
+//     break;
+//   case 'o':
+//     result += '0';
+//     break;
+//   default:
+//     result += str;
+// }
+// }
+// console.log(result);
+
+// <with forEach>
+
+// let arr = char.split('');
+// arr.forEach((item, index) => {
+// switch (item) {
+//   case 'a':
+//     arr[index] = '@';
+//     break;
+//   case 'e':
+//     arr[index] = '3';
+//     break;
+//   case 'g':
+//     arr[index] = '&';
+//     break;
+//   case 'i':
+//     arr[index] = '!';
+//     break;
+//   case 'o':
+//     arr[index] = '0';
+//     break;
+//   default:
+//     item;
+// }
+// });
+// console.log(arr.join(''));
 
